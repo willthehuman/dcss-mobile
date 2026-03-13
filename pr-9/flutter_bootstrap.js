75f89cab2918b179f9e50d8b15fa0a01a052d60d
willthehuman <willthehuman@users.noreply.github.com>
2026-03-13 16:41:37 +0000

    deploy: e26b073db45c8848ceb7dd9b7344f250155f124e

--- pr-9/flutter_bootstrap.js
+++ pr-9/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2053778620" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1535136109" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
